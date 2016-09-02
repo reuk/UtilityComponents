17fa0b826b4bd9da05792e5e0a7d8175195e94b8
reuk <reuben.thomas@me.com>
2016-09-02 12:16:58 +0100

    incremental updates

---
+++ src/scoped_thread.cpp
@@ -0,0 +1,12 @@
+#include "UtilityComponents/scoped_thread.hpp"
+
+scoped_thread::scoped_thread(std::thread&& t)
+        : t_(std::move(t)) {
+    if (!t_.joinable()) {
+        throw std::logic_error("scoped_thread: no thread");
+    }
+}
+
+scoped_thread::~scoped_thread() noexcept {
+    t_.join();
+}
